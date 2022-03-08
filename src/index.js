360bf3531e4077df7e79a2c261b4ab269eeb2de5
VinayMohanRawat <vinaymohan49@gmail.com>
2022-03-08 16:13:32 +0530

    Pending

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://vinaymohanrawat1:2DwN6pLmOugd33yo@cluster0.77vgl.mongodb.net/Middleware2?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
